ec84bc69f459b7ef583f2ae4d26a8b419c20a7d1
Erik Jishammar <erikjishammar@hotmail.com>
2025-10-13 19:54:24 +0200

    fix: minor change in server.ts

--- server.ts
+++ server.ts
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV !== "production"){
   app.use(vite.middlewares); 
 } else {
   app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, "dist/client/index.html"));
+  res.sendFile(join(__dirname, "client/index.html"));
 });
 }
 
